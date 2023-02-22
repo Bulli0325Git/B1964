933675128918cbc8d35da46fba0479e28c3e8350
saidevops <peketibullabbai@gmail.com>
2023-02-22 14:19:43 +0530

    files

--- file2.java
+++ file2.java
@@ -1,6 +1,10 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
  this is the java 2nd file with nothing file dk2
+=======
+ this is the java 2nd file with nothing file 2
+>>>>>>> ccdc02a (first commit)
 =======
  this is the java 2nd file with nothing file 2
 >>>>>>> ccdc02a (first commit)
