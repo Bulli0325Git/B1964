b220a5dbab043b1b18f373f09cf83fdef24c9334
saidevops <peketibullabbai@gmail.com>
2023-02-22 14:19:43 +0530

    cherry

--- file1.java
+++
@@ -1,7 +0,0 @@
-Hello Guys This is Java File 1
-contains hi messsage....
-<<<<<<< HEAD
-updating a information bullebbai
-java 1
-=======
->>>>>>> parent of ea5dbb4 (updatedfile1)
--- file2.java
+++ file2.java
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
  this is the java 2nd file with nothing file dk2
+=======
+ this is the java 2nd file with nothing file 2
+>>>>>>> ccdc02a (first commit)
