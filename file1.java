3d682cbe125641a23000991ce1c89644f1e35d65
saidevops <peketibullabbai@gmail.com>
2023-03-03 12:02:35 +0530

    resolved

--- file1.java
+++ file1.java
@@ -1,10 +1,8 @@
 Hello Guys This is Java File 1
 contains hi messsage....
-<<<<<<< HEAD
+HEAD
 updating a information bullebbai
 java 1
-=======
->>>>>>> parent of ea5dbb4 (updatedfile1)
-	>>>>>>> this is in the development branch updating the content
-	>>>>>>>
-	>>>>>>>for stash saving
+parent of ea5dbb4 (updatedfile1)
+	this is in the development branch updating the conten
+	for stash saving
